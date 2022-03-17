5f0972805ed915045a165d4108d2424423d7129d
Eliaslindh <ellilindh22@gmail.com>
2022-03-17 10:08:15 +0200

    update

--- optima_careerDay-master/public/script/app.js
+++ optima_careerDay-master/public/script/app.js
@@ -1,7 +1,7 @@
 const openSidebar = document.getElementById('button1')
 
 openSidebar.addEventListener('click', () => {
-    console.log('gmr')
+
     if (document.getElementById('sidebar').style.width == '200px') {
         document.getElementById('sidebar').style.width = '0px';
 
@@ -9,6 +9,23 @@ openSidebar.addEventListener('click', () => {
 
     else {
         document.getElementById('sidebar').style.width = '200px'
+
+    }
+
+})
+
+
+const closeSidebar = document.getElementById('close')
+
+closeSidebar.addEventListener('click', () => {
+
+    if (document.getElementById('sidebar').style.width == '0px') {
+        document.getElementById('sidebar').style.width = '200px';
+
+    }
+
+    else {
+        document.getElementById('sidebar').style.width = '0px'
 
     }
 
